4cef79adc2d0d65a222542832fa24f8ed86ef59e
Teerth Patel <teerth1205@gmail.com>
2020-02-28 20:42:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do 
     "My name is Teerth"
   end 
+  
+  get '/hometown' do 
+    "My hometown is Princeton"
+  end 
+  
+  get '/favortie-song' do 
+    "My favorite song is Runaway"
+  end 
 end
